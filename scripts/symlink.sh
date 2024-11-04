a4ac81c44f3ca22821d73e38fbb827aec5e9d1c2
Nikita Moshenskiy <nikita@cosmo.moshensky.com>
2024-11-04 02:45:01 +0200

    wip

---
+++ scripts/symlink.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Enable debugging
+# set -x
+set -eo pipefail
+
+# Set scripts full path to directory
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# -- Link .config items
+
+cd ../config
+config_files=$(ls -A)
+
+for file in $config_files; do
+	if [ -d "$file" ]; then
+		rm -rf "${HOME:?}/.config/$file"
+	fi
+
+	ln -sf "$DIR/../config/$file" "$HOME/.config/$file"
+done
