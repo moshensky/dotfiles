32613c5ca67a967ef051db7e4f88e73a9f12b945
Nikita Moshenskiy <nikita@cosmo.moshensky.com>
2024-11-05 10:46:13 +0200

    wip

--- config/nvim/lua/kickstart/plugins/lsp.lua
+++ config/nvim/lua/kickstart/plugins/lsp.lua
@@ -215,7 +215,15 @@ return {
       --    :Mason
       --
       --  You can press `g?` for help in this menu.
-      require('mason').setup()
+      require('mason').setup {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      }
 
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
