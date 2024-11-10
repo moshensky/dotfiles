463e01a0d694096dc882b2869d166380747541c6
Nikita Moshenskiy <nikita@moshensky.com>
2024-11-10 19:44:04 +0200

    wip

--- config/awesome/deco/statusbar.lua
+++ config/awesome/deco/statusbar.lua
@@ -7,7 +7,12 @@ local _M = {}
 -- Custom Local Library: Common Functional Decoration
 local deco = {
 	wallpaper = require("deco.wallpaper"),
+	taglist = require("deco.taglist"),
+	tasklist = require("deco.tasklist"),
 }
+
+local taglist_buttons = deco.taglist()
+local tasklist_buttons = deco.tasklist()
 
 -- Wibar
 
@@ -20,8 +25,18 @@ awful.screen.connect_for_each_screen(function(s)
 	-- Create an imagebox widget
 
 	-- Create a taglist widget
+	s.mytaglist = awful.widget.taglist({
+		screen = s,
+		filter = awful.widget.taglist.filter.all,
+		buttons = taglist_buttons,
+	})
 
 	-- Create a tasklist widget
+	s.mytasklist = awful.widget.tasklist({
+		screen = s,
+		filter = awful.widget.tasklist.filter.currenttags,
+		buttons = tasklist_buttons,
+	})
 
 	-- Create the wibox
 
