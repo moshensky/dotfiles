6a9d5c3504c3a0a75f83a39798a5e1dd6792a24a
Nikita Moshenskiy <nikita@moshensky.com>
2024-11-10 15:23:32 +0200

    wip

---
+++ config/awesome/main/menu.lua
@@ -0,0 +1,57 @@
+-- Standard awesome library
+local awful = require("awful")
+local hotkeys_popup = require("awful.hotkeys_popup").widget
+-- Theme handling library
+local beautiful = require("beautiful") -- for awesome.icon
+
+local M = {} -- menu
+local _M = {} -- module
+
+-- reading
+-- https://awesomewm.org/apidoc/popups%20and%20bars/awful.menu.html
+
+-- This is used later as the default terminal and editor to run.
+-- local terminal = "xfce4-terminal"
+local terminal = RC.vars.terminal
+
+-- Variable definitions
+-- This is used later as the default terminal and editor to run.
+local editor = RC.vars.editor
+local editor_cmd = RC.vars.editor_cmd
+
+M.awesome = {
+	{
+		"hotkeys",
+		function()
+			hotkeys_popup.show_help(nil, awful.screen.focused())
+		end,
+	},
+	{ "manual", terminal .. " -e man awesome" },
+	{ "edit config", editor_cmd .. " " .. awesome.conffile },
+	{ "Terminal", terminal },
+	-- TODO: Checkout what is oblogout and configure someting
+	{ "Shutdown/Logout", "oblogout" },
+	{ "restart", awesome.restart },
+	{
+		"quit",
+		function()
+			awesome.quit()
+		end,
+	},
+}
+
+function _M.get()
+	-- Main Menu
+	local menu_items = {
+		{ "awesome", M.awesome, beautiful.awesome_subicon },
+		{ "open terminal", terminal },
+	}
+
+	return menu_items
+end
+
+return setmetatable({}, {
+	__call = function(_, ...)
+		return _M.get(...)
+	end,
+})
--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -27,7 +27,7 @@ require("main.error-handling")
 RC = {} -- global namespace, on top before require any modules
 RC.vars = require("main.user-variables")
 
-modkey = RC.vars.modkey
+local modkey = RC.vars.modkey
 
 local main = {
 	layouts = require("main.layouts"),
@@ -36,30 +36,20 @@ local main = {
 	rules = require("main.rules"),
 }
 
--- {{{ Variable definitions
+-- Layouts
+RC.layouts = main.layouts()
+awful.layout.layouts = RC.layouts
+
+-- Tags
+RC.tags = main.tags()
+
+-- Menu
+RC.mainmenu = awful.menu({ items = main.menu() })
+RC.launcher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = RC.mainmenu })
+menubar.utils.terminal = RC.vars.terminal
+
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init("~/.config/awesome/theme.lua")
-
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-	awful.layout.suit.tile,
-	awful.layout.suit.floating,
-	-- awful.layout.suit.tile.left,
-	-- awful.layout.suit.tile.bottom,
-	-- awful.layout.suit.tile.top,
-	-- awful.layout.suit.fair,
-	-- awful.layout.suit.fair.horizontal,
-	-- awful.layout.suit.spiral,
-	-- awful.layout.suit.spiral.dwindle,
-	awful.layout.suit.max,
-	awful.layout.suit.max.fullscreen,
-	-- awful.layout.suit.magnifier,
-	-- awful.layout.suit.corner.nw,
-	-- awful.layout.suit.corner.ne,
-	-- awful.layout.suit.corner.sw,
-	-- awful.layout.suit.corner.se,
-}
--- }}}
 
 -- {{{ Menu
 -- Create a launcher widget and a main menu
@@ -181,24 +171,9 @@ local tasklist_buttons = gears.table.join(
 	end)
 )
 
-local function set_wallpaper(s)
-	-- Wallpaper
-	if beautiful.wallpaper then
-		local wallpaper = beautiful.wallpaper
-		-- If wallpaper is a function, call it with the screen
-		if type(wallpaper) == "function" then
-			wallpaper = wallpaper(s)
-		end
-		gears.wallpaper.maximized(wallpaper, s, true)
-	end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
 	-- Wallpaper
-	set_wallpaper(s)
+	-- set_wallpaper(s)
 
 	-- Each screen has its own tag table.
 	awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
