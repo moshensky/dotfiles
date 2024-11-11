b1be95540d8879e1797808c280f3f79f40a031de
Nikita Moshenskiy <nikita@moshensky.com>
2024-11-11 15:59:10 +0200

    wip

--- config/awesome/deco/statusbar.lua
+++ config/awesome/deco/statusbar.lua
@@ -17,8 +17,25 @@ local taglist_buttons = deco.taglist()
 local tasklist_buttons = deco.tasklist()
 
 -- Awesome-wm-widgets
+local mk_batteryarc_widget = require("awesome-wm-widgets.batteryarc-widget.batteryarc")
+local mk_cpu_widget = require("awesome-wm-widgets.cpu-widget.cpu-widget")
+local mk_net_speed_widget = require("awesome-wm-widgets.net-speed-widget.net-speed")
+local mk_ram_widget = require("awesome-wm-widgets.ram-widget.ram-widget")
 local mk_volume_widget = require("awesome-wm-widgets.volume-widget.volume")
 
+local batteryarc_widget = mk_batteryarc_widget({
+	show_current_level = true,
+	-- arc_thickness = 1,
+})
+local cpu_widget = mk_cpu_widget({
+	width = 70,
+	step_width = 2,
+	step_spacing = 0,
+	color = "#434c5e",
+	enable_kill_button = true,
+})
+local net_speed_widget = mk_net_speed_widget()
+local ram_widget = mk_ram_widget()
 local volume_widget = mk_volume_widget({
 	widget_type = "arc", -- horizontal_bar, vertical_bar, icon, icon_and_text, arc
 })
@@ -125,6 +142,10 @@ awful.screen.connect_for_each_screen(function(s)
 			layout = wibox.layout.fixed.horizontal,
 			cpuwidget,
 			memwidget,
+			batteryarc_widget,
+			cpu_widget,
+			net_speed_widget,
+			ram_widget,
 			volume_widget,
 			mykeyboardlayout,
 			wibox.widget.systray(),
