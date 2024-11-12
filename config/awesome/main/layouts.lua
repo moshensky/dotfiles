5b94a684884cc37811add10611b3a2c19dc495e3
Nikita Moshenskiy <nikita@moshensky.com>
2024-11-12 17:28:47 +0200

    wip

--- config/awesome/main/layouts.lua
+++ config/awesome/main/layouts.lua
@@ -6,10 +6,10 @@ local _M = {}
 function _M.get()
 	-- Table of layouts to cover with awful.layout.inc, order matters.
 	local layouts = {
-		awful.layout.suit.spiral.dwindle,
-		awful.layout.suit.tile,
-		awful.layout.suit.floating,
 		awful.layout.suit.tile.left,
+		awful.layout.suit.tile,
+		-- awful.layout.suit.floating,
+		awful.layout.suit.spiral.dwindle,
 		-- awful.layout.suit.tile.bottom,
 		-- awful.layout.suit.tile.top,
 		-- awful.layout.suit.fair,
