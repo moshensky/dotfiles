e52fab36109c5786f0c31feb1694a0f9daa61d8e
Nikita Moshenskiy <nikita@moshensky.com>
2024-12-03 10:27:32 +0200

    wip

--- config/nvim/lua/custom/plugins/telescope.nvim.lua
+++ config/nvim/lua/custom/plugins/telescope.nvim.lua
@@ -44,15 +44,24 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
+    --
+
+    local actions = require 'telescope.actions'
     require('telescope').setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = {
+            -- ['<c-enter>'] = 'to_fuzzy_refine',
+            ['dd'] = actions.delete_buffer,
+          },
+          n = {
+            ['dd'] = actions.delete_buffer,
+          },
+        },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
